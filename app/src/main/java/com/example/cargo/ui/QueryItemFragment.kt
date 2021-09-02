2ad6aaf5d8c4d213c786fc9e733e81efec41895f
Anuj <anujrai04082002.0@gmail.com>
2021-09-02 13:42:39 +0530

    Improved Dialog Bar layout status:Successfully

--- app/src/main/java/com/example/cargo/ui/QueryItemFragment.kt
+++ app/src/main/java/com/example/cargo/ui/QueryItemFragment.kt
@@ -1,16 +1,13 @@
 package com.example.cargo.ui
 
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
-import android.view.WindowManager
 import androidx.fragment.app.Fragment
+import com.example.cargo.R
 import com.example.cargo.databinding.QueryItemFragmentBinding
 import com.example.cargo.utils.DialogBox
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-import android.app.AlertDialog
-import com.example.cargo.R
 
 
 @AndroidEntryPoint
@@ -28,15 +25,6 @@ class QueryItemFragment : Fragment(R.layout.query_item_fragment) {
     }
 
     private fun openDialog() {
-        val view: View = layoutInflater.inflate(R.layout.custom_dialog_fragment, null)
-        val infoDialog: AlertDialog = AlertDialog.Builder(activity)
-            .setView(view)
-            .create()
-        val window = infoDialog.window
-        val wlp: WindowManager.LayoutParams = window?.attributes!!
-        wlp.gravity = Gravity.TOP
-        wlp.flags = wlp.flags and WindowManager.LayoutParams.FLAG_DIM_BEHIND.inv()
-        window.attributes = wlp
-        infoDialog.show()
+        dialogBox.showDialog(requireActivity())
     }
 }
--- app/src/main/java/com/example/cargo/utils/DialogBox.kt
+++ app/src/main/java/com/example/cargo/utils/DialogBox.kt
@@ -1,24 +1,24 @@
 package com.example.cargo.utils
 
 import android.app.AlertDialog
-import android.app.Dialog
-import android.os.Bundle
-import android.util.Log
+import android.content.Context
 import android.view.Gravity
-import android.view.Window
+import android.view.LayoutInflater
 import android.view.WindowManager
-import androidx.fragment.app.DialogFragment
-import com.example.cargo.TAG
 import com.example.cargo.databinding.CustomDialogFragmentBinding
 import javax.inject.Inject
 
-class DialogBox @Inject constructor() : DialogFragment() {
-    private lateinit var binding: CustomDialogFragmentBinding
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        binding = CustomDialogFragmentBinding.inflate(layoutInflater)
-        val alertDialog =
-            AlertDialog.Builder(requireActivity()).setView(binding.root)
-
-        return alertDialog.create()
+class DialogBox @Inject constructor()  {
+    fun showDialog(context: Context){
+        val binding = CustomDialogFragmentBinding.inflate(LayoutInflater.from(context))
+        val infoDialog: AlertDialog = AlertDialog.Builder(context)
+            .setView(binding.root)
+            .create()
+        val window = infoDialog.window
+        val wlp: WindowManager.LayoutParams = window?.attributes!!
+        wlp.gravity = Gravity.TOP
+        wlp.flags = wlp.flags and WindowManager.LayoutParams.FLAG_DIM_BEHIND.inv()
+        window.attributes = wlp
+        infoDialog.show()
     }
 }
