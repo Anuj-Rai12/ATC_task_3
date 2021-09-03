a29815891c0b07c933e4923b2727711d2ce34ee7
Anuj <anujrai04082002.0@gmail.com>
2021-09-03 12:16:54 +0530

    Removed Some Bug status:Successfully

--- app/src/main/java/com/example/cargo/MainActivity.kt
+++ app/src/main/java/com/example/cargo/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.cargo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.example.cargo.databinding.ActivityMainBinding
+import com.example.cargo.utils.setColor
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
+        window?.statusBarColor=this.setColor(R.color.app_color)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
